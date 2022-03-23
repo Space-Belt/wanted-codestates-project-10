c4dd1594cb83ea4be63bdbbb6daea90e587c5bdf
Space-Belt <dngurwn@gmail.com>
2022-03-23 23:08:40 +0900

    [Feat] 추천검색어 UI

--- src/components/SearchList.jsx
+++ src/components/SearchList.jsx
@@ -1,14 +1,40 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
+import { useMediaQuery } from 'react-responsive';
+import styled from 'styled-components';
+import { BiSearch } from 'react-icons/bi';
 
 function SearchList() {
-  const searchWord = useSelector((state) => state.search.searchDisease);
-  console.log(searchWord);
+  // const searchWord = useSelector((state) => state.search.searchDisease);
+  const responsiveWeb = useMediaQuery({ query: '(min-width: 1040px)' });
   return (
-    <ul>
-      <li>암</li>
-    </ul>
+    <RecommendBox responsiveWeb={responsiveWeb}>
+      <RecommendWord>추천검색어</RecommendWord>
+      <Diseases>
+        <li>
+          <BiSearch />암
+        </li>
+      </Diseases>
+    </RecommendBox>
   );
 }
+
+const RecommendBox = styled.div`
+  width: ${({ responsiveWeb }) => (responsiveWeb ? '660px' : 'calc(100vw - 40px)')};
+  height: 400px;
+  margin-top: 7px;
+  border-radius: 20px;
+  background-color: #fff;
+  padding: 30px;
+`;
+
+const RecommendWord = styled.span`
+  color: #747b81;
+`;
+
+const Diseases = styled.ul`
+  margin-top: 15px;
+  width: 100%;
+`;
 
 export default SearchList;
